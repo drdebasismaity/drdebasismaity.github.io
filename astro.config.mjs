5cc4de2fa26bf05b7524b28864810e3d83f4d725
Agnibho Mondal <mondal@agnibho.com>
2025-03-27 00:57:18 +0530

    URL Typo Correction

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from 'astro/config';
 import remarkImgLinks from '@pondorasti/remark-img-links';
 
 const isProduction = process.env.NODE_ENV === 'production'
-const siteUrl = isProduction ? "https://dmaity.github.io/dmaity/" : "http://localhost:4321/dmaity/"
+const siteUrl = isProduction ? "https://drdebasismaity.github.io/dmaity/" : "http://localhost:4321/dmaity/"
 
 // https://astro.build/config
 export default defineConfig({
   site: siteUrl,
-  base: '/dmaity',
+  base: '/dmaity/',
   markdown : {
     remarkPlugins: [
       [remarkImgLinks, {absolutePath: siteUrl}]
